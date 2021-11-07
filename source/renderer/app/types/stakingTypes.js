31ed1bdcffab668e388924579f50877b97dff224
PANL Stake Pool <83083099+rp5210@users.noreply.github.com>
2021-11-07 16:29:18 -0500

    Update stakingTypes.js

--- source/renderer/app/types/stakingTypes.js
+++ source/renderer/app/types/stakingTypes.js
@@ -1,5 +1,5 @@
 // @flow
 
 export type RedeemItnRewardsStep = 'configuration' | 'confirmation' | 'result';
-export type SmashServerType = 'iohk' | 'custom' | 'direct' | 'none';
+export type SmashServerType = 'iohk' | 'custom' | 'direct' | 'none' | 'xSPO' | 'Single Pool Alliance' | 'Mission Driven';
 export type DelegationAction = 'join' | 'quit';
